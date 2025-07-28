d3e76256f175fb9381fe859d60c481485849e529
João Paulo Paggi <nullifidianz@gmail.com>
2025-07-28 15:49:08 -0300

    Adiciona configurações de segurança e banco de dados ao arquivo application.properties

---
+++ src/main/java/com/nullifidianz/java_auth/entity/User.java
@@ -0,0 +1,33 @@
+package com.nullifidianz.java_auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "user_table")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String nome;
+    @Column(name = "user_email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "user_email", nullable = false)
+    private String senha;
+
+}
