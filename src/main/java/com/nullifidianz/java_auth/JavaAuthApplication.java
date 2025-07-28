bd4a0dbe78d7d521da9b6ee4dbf591406ef3ea1c
João Paulo Paggi <nullifidianz@gmail.com>
2025-07-28 11:55:19 -0300

    initial commit

---
+++ src/main/java/com/nullifidianz/java_auth/JavaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.java_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nullifidianz/java_auth/JavaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.java_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
